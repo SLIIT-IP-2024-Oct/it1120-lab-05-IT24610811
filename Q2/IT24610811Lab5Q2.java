21e4cda150fbfd58657529b786f6568039ce3054
IT24610811 <it24610811@my.sliit.lk>
2024-11-27 17:36:36 +0530

    Add files via upload

---
+++ Q2/IT24610811Lab5Q2.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+public class IT24610811Lab5Q2 {
+    public static void main(String[] args) {
+        
+        Scanner scanner = new Scanner(System.in);
+        
+      
+        System.out.print("Enter the number of new members you have introduced: ");
+        int newMembers = scanner.nextInt();
+        
+     
+        if (newMembers < 0) {
+            System.out.println("Invalid input. The number of new members cannot be negative.");
+        } else {
+          
+            String prize;
+            switch (newMembers) {
+                case 0:
+                    prize = "No Prize";
+                    break;
+                case 1:
+                    prize = "Pen";
+                    break;
+                case 2:
+                    prize = "Umbrella";
+                    break;
+                case 3:
+                    prize = "Bag";
+                    break;
+                case 4:
+                    prize = "Travelling Chair";
+                    break;
+                default:
+                    prize = "Headphone"; 
+            }
+            
+            
+            System.out.println("You are entitled to a: " + prize);
+        }
+        
+        
+        scanner.close();
+    }
+}
